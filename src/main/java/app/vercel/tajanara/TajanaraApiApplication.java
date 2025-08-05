cdeffafdac12b94c7da1f726ffbd13f33dd07e6d
kimdoyeob <ga.miro3721@gmail.com>
2025-08-05 10:26:46 +0900

    Initial commit

---
+++ src/main/java/app/vercel/tajanara/TajanaraApiApplication.java
@@ -0,0 +1,13 @@
+package app.vercel.tajanara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TajanaraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TajanaraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/vercel/tajanara/TajanaraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vercel.tajanara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TajanaraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
